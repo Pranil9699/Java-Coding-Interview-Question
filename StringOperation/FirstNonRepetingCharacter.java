188efeff9cfcade400df0a984540a78b8118e337
Pranil Takawane <99954777+Pranil9699@users.noreply.github.com>
2025-07-27 22:24:07 +0530

    Create FirstNonRepetingCharacter.java

---
+++ StringOperation/FirstNonRepetingCharacter.java
@@ -0,0 +1,18 @@
+public class FirstNonRepetingCharacter {
+    public static void main(String[] args) {
+        String str="PranilP";
+        for(int i = 0; i< str.length();i++){
+            int count=0;
+            for(int j=i+1; j<str.length();j++){
+                
+                if(str.charAt(i)==str.charAt(j))
+                count++;
+                
+            }
+            if(count==0){
+            System.out.println(str.charAt(i));
+            break;
+            }
+        }
+    }
+}
