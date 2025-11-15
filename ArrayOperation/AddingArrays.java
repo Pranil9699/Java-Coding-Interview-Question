9e120051c48ade4005ba4dce060e7647fbd11f69
Pranil Takawane <99954777+Pranil9699@users.noreply.github.com>
2025-11-15 08:56:07 +0530

    Implement method to add and print two arrays
    Added a new method to combine and print two integer arrays.

--- ArrayOperation/AddingArrays.java
+++ ArrayOperation/AddingArrays.java
@@ -15,6 +15,24 @@ public class AddingArrays {
 		for (String string : CombinedArray) {
 			System.out.println(string);
 		}
+		main1();
 
 	}
+	public static void main1() {
+        int array1[]={1,2,3,4,5};
+        int array2[]={5,6,7,8,9,0};
+        int addingArray[]=new int[array1.length+array2.length];
+        int i;
+        int j=0;
+        for(i=0;i<addingArray.length;i++){
+            if(i>=array1.length){
+              addingArray[i]= array2[j++];
+            }else{
+              addingArray[i]= array1[i];
+            }
+        }
+        for(int k=0;k<addingArray.length;k++){
+            System.out.print(addingArray[k]+",");
+        }
+    }
 }
